3354c191c90db470fc593f97858eaa39bc83fb5e
Magdesyan_Kirill <107571018+2913090@users.noreply.github.com>
2023-09-19 14:37:58 +0300

    Add files via upload

---
+++ calc_if_else_if.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+int main()
+{
+    using namespace std;
+    setlocale(LC_ALL, "");
+    float q, w;
+    char e;
+    cout << "Введите 1-е число\n";
+    cin >> q;
+    cout << "Введите 2-е число\n";
+    cin >> w;
+    cout << "Введите действие(+,-,*,/)\n";
+    cin >> e;
+    cout << q << e << w << "=";
+    if (e == '/') {
+        if (w == 0)
+            cout << "Ошибка (на 0 делить нельзя)";
+        else
+            cout << q / w;
+    }
+    else if (e == '*') 
+        cout << q * w;
+    else if (e == '+')
+        cout << q + w;
+    else if (e == '-')
+        cout << q - w;
+    else 
+        cout << "Введенные данные не корректны";
+}
