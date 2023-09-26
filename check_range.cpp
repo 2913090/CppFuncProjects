b5115865e3721bedf592050adf3dbd121bb2e172
Magdesyan_Kirill <107571018+2913090@users.noreply.github.com>
2023-09-26 14:42:48 +0300

    Add files via upload

---
+++ check_range.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "");
+    int q,w,e;
+    cout << "Введите цифру\n";
+    cin >> q;
+    cout << "Введите нижнюю границу диапозона\n";
+    cin >> w;
+    cout << "Введите верхнюю границу диапозона\n";
+    cin >> e;
+    if (w > e) 
+        swap(w, e);
+    (q >= w && q <= e) ? cout << "Введенное значение лежит между " << w << " и " << e : cout << "Введенное значение не лежит между " << w << " и " << e;
+
+}
